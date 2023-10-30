cb3572a01db19a2d09cdb2cb027afc5f1359de76
Mateen Khan <mateenmk2428@gmail.com>
2023-10-30 12:44:27 +0530

    This is the commit for web application

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
+# Load the necessary libraries
 import pickle as pkl
 import pandas as pd
 import numpy as np
-from Flask import Flask,request,app,jsonify, url_for, render_template
+from flask import Flask,request,app,jsonify, url_for, render_template
 
+# Load the Model
 app = Flask(__name__)
 regmodel = pkl.load(open('regmodel.pkl','rb'))
+scaler = pkl.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/predict_api', methods = ['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__ == '__main__':
+    app.run(debug=True)
